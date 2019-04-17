65978ce71aeb7346e9762ee3388cfc0efd8db624
Luis Cabrera Benito <contacto@parzibyte.me>
2019-04-16 23:44:24 -0500

    Arreglado error de escritura; pero igualmente funcionaba porque se tomaba el campo de la constante

--- app/src/main/java/me/parzibyte/agenda/Utilerias.java
+++ app/src/main/java/me/parzibyte/agenda/Utilerias.java
@@ -5,7 +5,7 @@ public class Utilerias {
     public static final String NOMBRE_BD = "agenda";
     public static final String CAMPO_ID = "id";
     public static final String CAMPO_NOMBRE = "nombre";
-    public static final String CAMPO_TELEFONO = "persona";
+    public static final String CAMPO_TELEFONO = "telefono";
     public static final String CREAR_TABLA_PERSONA = "create table "
             + TABLA_PERSONA
             + " (" + CAMPO_ID + " INTEGER, "
